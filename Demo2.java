67e5ced66e5f0c731668477b186777f1b8d3e4f4
Dewuruge <dewurugekalpadeep@gmail.com>
2023-07-28 13:40:02 +0530

    Demo2.java

--- Demo2.java
+++ Demo2.java
@@ -1,6 +1,7 @@
-public class Demo{
+public class Demo2 {
     public static void main(String[] args) {
         System.out.println("Hellp GitHub!");
         System.out.println("I am from Demo2");
     }
+    
 }
